973522031065f7b634ce33262e521c357c6c8c7b
mzikri0604200 <muhzikri06@gmail.com>
2025-11-24 10:50:34 +0700

    Add Component - Header with Daisy UI - Footer with Daisy UI

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <RouterView />
+  <Header />
+  <main class="mx-auto mx-w-6xl px-8 py-20 my-15 min-h-[80vh]">
+    <RouterView />
+  </main>
+  <Footer />
 </template>
