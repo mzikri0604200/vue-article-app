8d9f504e1af148ec1074899dc6b58dd59651f803
mzikri0604200 <muhzikri06@gmail.com>
2025-12-08 15:22:58 +0700

    Add Component ThemeSelector

---
+++ src/components/ThemeSelector.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useThemeStore } from '@/store/themeStore'
+const themeStore = useThemeStore()
+
+function changeTheme(e) {
+	const value = e.target.value
+	themeStore.setTheme(value)
+}
+</script>
+
+<template>
+	<div class="font-control mx-2">
+		<select class="select select-border" :value="themeStore.theme" @change="changeTheme">
+			<option disabled selected>Pick a color</option>
+			<option v-for="theme in themeStore.availableThemes" :key="theme" :value="theme">{{ theme }}</option>
+		</select>
+	</div>
+</template>
